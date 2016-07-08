ecb9dfe4958ab44bab295a48e2618cd2d96422d2
Rob Guglielmetti <rob.guglielmetti@nrel.gov>
2016-07-08 15:46:49 -0600

    can you hear me now, simplecov?

--- test/coverage.rb
+++ test/coverage.rb
@@ -1,8 +1,11 @@
 require 'simplecov'
+require 'coveralls'
+
 # save to CircleCI's artifacts directory if we're on CircleCI
 if ENV['CIRCLE_ARTIFACTS']
   dir = File.join(ENV['CIRCLE_ARTIFACTS'], "coverage")
   SimpleCov.coverage_dir(dir)
 end
 
+SimpleCov.formatter = Coveralls::SimpleCov::Formatter
 SimpleCov.start
