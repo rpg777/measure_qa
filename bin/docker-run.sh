f621526c31c802fed0927200b61f93cd7bdbd923
Rob Guglielmetti <rob.guglielmetti@nrel.gov>
2016-07-08 16:54:17 -0600

    tracking files

--- bin/docker-run.sh
+++ bin/docker-run.sh
@@ -5,6 +5,8 @@ export CIRCLECI=true
 export PATH="$HOME/.rbenv/bin:$PATH"
 eval "$(rbenv init -)"
 
+echo eval "$(cd "$(ruby -e "File.join(ENV[:CIRCLE_ARTIFACTS.to_s], :coveralls.to_s)" && ls -al)"
+
 # Run a specific set of tests on each node.
 # Test groups are defined in the Rakefile.
 # Each group must have a total runtime less
@@ -19,6 +21,7 @@ bundle install
 rake test:example2
 bundle exec rake rubocop
 
+echo eval "$(cd "$(ruby -e "File.join(ENV[:CIRCLE_ARTIFACTS.to_s], :coveralls.to_s)" && ls -al)"
 
 # case $CIRCLE_NODE_INDEX in
 #   0)
