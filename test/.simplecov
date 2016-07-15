5b5636f61e3389517746f528d9cef4dab26c0d8a
Rob Guglielmetti <rob.guglielmetti@nrel.gov>
2016-07-15 16:03:59 -0600

    and now for something completely different...

---
+++ test/.simplecov
@@ -0,0 +1,5 @@
+#. simplecov
+SimpleCov.start do
+  
+  # any custom configs like groups and filters can be here at a central place
+end
--- test/bin/docker-run.sh
+++ test/bin/docker-run.sh
@@ -13,7 +13,7 @@ eval "$(rbenv init -)"
 # Each group must have a total runtime less
 # than 2 hrs.
 
-cd /measure_qa
+#cd /measure_qa
 
 # test crap
 echo "RUBYLIB = $RUBYLIB"
