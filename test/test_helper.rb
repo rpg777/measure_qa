df6952e6902d6e244721aced4865194e9797d925
Rob Guglielmetti <rob.guglielmetti@nrel.gov>
2016-07-15 14:45:16 -0600

    fix deprecation warning, filter

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -13,10 +13,10 @@ puts "env is: #{::ENV.inspect}"
 # Get the code coverage in html for local viewing
 # and in JSON for coveralls
 #SimpleCov.formatter = Coveralls::SimpleCov::Formatter
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::HTMLFormatter,
   Coveralls::SimpleCov::Formatter
-]
+])
 
 # save to CircleCI's artifacts directory if we're on CircleCI
 if ENV['CIRCLE_ARTIFACTS']
@@ -29,7 +29,7 @@ end
 
 # Ignore some of the code in coverage testing
 SimpleCov.start do 
-  add_filter '/measures/.*/tests/'
+  add_filter '/measures/**/tests/'
 end
 
 #$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
