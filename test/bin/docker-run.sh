194439e1fdfc5b768c40e6c1ebdf0ea9345fa539
Rob Guglielmetti <rob.guglielmetti@nrel.gov>
2016-09-27 15:31:01 -0600

    still broke

--- test/bin/docker-run.sh
+++ test/bin/docker-run.sh
@@ -18,6 +18,8 @@ echo "Running tests with RUBYLIB = $RUBYLIB"
 #bundle update
 bundle install
 
+# sldjfsldkj
+
 ruby -Ilib -e 'ARGV.each { |f| require f }' ./measures/parsed/**/tests/*_[tT]est.rb
 
 #bundle exec rake test:all
